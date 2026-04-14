a655536769c852a3bb0a9eb233e064ffae758662
Kea Abaquita <kea.abaquita.23@usjr.edu.ph>
2026-04-14 14:58:01 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import DashboardLayout from './components/DashboardLayout';
->>>>>>> a531a76ee5770ed6feb1cd3bb3d52089790b7cb3
 import StudentDashboard from './components/StudentDashboard';
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      {/* <Login /> */}
-      <StudentDashboard />
-    </div>
-=======
     <Router>
       <div className="App">
         <Routes>
@@ -28,7 +19,6 @@ function App() {
         </Routes>
       </div>
     </Router>
->>>>>>> a531a76ee5770ed6feb1cd3bb3d52089790b7cb3
   );
 }
 
